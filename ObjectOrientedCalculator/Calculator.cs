d1480930002e83180ee9e2cfa0952d7400bdfb02
Victor Muniz <victor.hm2000@aluno.ifsc.edu.br>
2021-04-20 16:58:30 -0300

    Primeiro Commit

---
+++ ObjectOrientedCalculator/Calculator.cs
@@ -0,0 +1,47 @@
+﻿namespace ObjectOrientedCalculator
+{
+    class Calculator
+    {
+        public double firstNumber;
+        public double secondNumber;
+        public double result;
+        public string operationHistory = "##### Operations #####\n";
+
+
+        public double Add()
+        {
+            result = firstNumber + secondNumber;
+            operationHistory += $"{firstNumber} + {secondNumber} = " + result.ToString() + "\n";
+            return result;
+        }
+
+        public double Sub()
+        {
+            result = firstNumber - secondNumber;
+            operationHistory += $"{firstNumber} - {secondNumber} = " + result.ToString() + "\n";
+            return result;
+        }
+
+        public double Multiple()
+        {
+            result = firstNumber * secondNumber;
+            operationHistory += $"{firstNumber} * {secondNumber} = " + result.ToString() + "\n";
+            return result;
+        }
+
+        public double Division()
+        {
+            if(secondNumber != 0)
+            {
+                result = firstNumber / secondNumber;
+                operationHistory += $"{firstNumber} / {secondNumber} = " + result.ToString() + "\n";
+            }
+            return result;
+        }
+
+        public void ShowOperations()
+        {
+            
+        }
+    }
+}
---
+++ ObjectOrientedCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator1 = new Calculator();
+            calculator1.firstNumber = 3;
+            calculator1.secondNumber = 5;
+            
+            calculator1.Add();
+            calculator1.Sub();
+            calculator1.Multiple();
+            calculator1.Division();
+
+            Console.WriteLine(calculator1.operationHistory);
+            Console.ReadLine();
+        }
+
+    }
+}
